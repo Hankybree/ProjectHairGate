aa02078c602ad1d0274eff9b9d91af29617b33ef
johannesryberg <johannes.ryberg@gmail.com>
2019-11-26 11:02:55 +0100

    implementation of GridRecyclerViewn only in java, so not done yet

--- app/src/main/java/com/example/projecthairgate/GalleryActivity.java
+++ app/src/main/java/com/example/projecthairgate/GalleryActivity.java
@@ -2,10 +2,16 @@ package com.example.projecthairgate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.GridLayoutAnimationController;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -125,6 +131,68 @@ public class GalleryActivity extends AppCompatActivity {
         }
     });
 
+    }
+
+    /**
+     * An extension of RecyclerView, focused more on resembling a GridView.
+     * Unlike {@link android.support.v7.widget.RecyclerView}, this view can handle
+     * {@code <gridLayoutAnimation>} as long as you provide it a
+     * {@link android.support.v7.widget.GridLayoutManager} in
+     * {@code setLayoutManager(LayoutManager layout)}.
+     * <p/>
+     * Created by Freddie (Musenkishi) Lust-Hed.
+     */
+    public class GridRecyclerView extends RecyclerView {
+
+        public GridRecyclerView(Context context) {
+            super(context);
+        }
+
+        public GridRecyclerView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public GridRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+        }
+
+        @Override
+        public void setLayoutManager(LayoutManager layout) {
+            if (layout instanceof GridLayoutManager) {
+                super.setLayoutManager(layout);
+            } else {
+                throw new ClassCastException("You should only use a GridLayoutManager with GridRecyclerView.");
+            }
+        }
+
+        @Override
+        protected void attachLayoutAnimationParameters(View child, @NonNull ViewGroup.LayoutParams params, int index, int count) {
+
+            if (getAdapter() != null && getLayoutManager() instanceof GridLayoutManager) {
+
+                GridLayoutAnimationController.AnimationParameters animationParams =
+                        (GridLayoutAnimationController.AnimationParameters) params.layoutAnimationParameters;
+
+                if (animationParams == null) {
+                    animationParams = new GridLayoutAnimationController.AnimationParameters();
+                    params.layoutAnimationParameters = animationParams;
+                }
+
+                int columns = ((GridLayoutManager) getLayoutManager()).getSpanCount();
+
+                animationParams.count = count;
+                animationParams.index = index;
+                animationParams.columnsCount = columns;
+                animationParams.rowsCount = count / columns;
+
+                final int invertedIndex = count - 1 - index;
+                animationParams.column = columns - 1 - (invertedIndex % columns);
+                animationParams.row = animationParams.rowsCount - 1 - invertedIndex / columns;
+
+            } else {
+                super.attachLayoutAnimationParameters(child, params, index, count);
+            }
+        }
     }
 }
 
