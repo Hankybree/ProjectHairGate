874467860e209b62aff84eda60212822b30224a2
Henrik Frank <frank.havsvinge@gmail.com>
2019-11-28 14:35:24 +0100

    Added camera code. TODO Integrate

--- app/src/main/java/com/example/projecthairgate/GalleryActivity.java
+++ app/src/main/java/com/example/projecthairgate/GalleryActivity.java
@@ -1,16 +1,23 @@
 package com.example.projecthairgate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,10 +25,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GalleryActivity extends AppCompatActivity {
+
+    public final int CAMERA_REQUEST_CODE = 1;
+    private String picturePath;
 
     private FirebaseDatabase mRoot;
     private DatabaseReference mRef;
@@ -46,7 +58,6 @@ public class GalleryActivity extends AppCompatActivity {
 
         images = new ArrayList<>();
     }
-
 
     @Override
     protected void onStart() {
@@ -75,7 +86,82 @@ public class GalleryActivity extends AppCompatActivity {
 
     }
 
-    public void onClick(View view) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap image = BitmapFactory.decodeFile(picturePath);
+
+                // TODO face swap-code
+            }
+        }
+    }
+
+    public void onClickTakePicture(View view)
+    {
+        showCamera();
+    }
+
+    private void showCamera()
+    {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(cameraIntent.resolveActivity(getPackageManager()) != null)
+        {
+            File imageFile;
+
+            try
+            {
+                imageFile = createImageFile();
+            }
+            catch (IOException e)
+            {
+                return;
+            }
+
+            if(imageFile != null)
+            {
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.projecthairgate.fileprovider", imageFile);
+
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException
+    {
+        File image;
+
+        // Path to images in phone
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        // New image folder
+        File imageDir = new File(dir, "HairImages");
+
+        // Check if folders exist
+        if(!imageDir.exists())
+        {
+            if(!imageDir.mkdirs())
+            {
+                Toast toast = Toast.makeText(this, "Could not create folder", Toast.LENGTH_LONG);
+
+                toast.show();
+            }
+        }
+
+        // Create the image file
+        image = File.createTempFile("hairstyle", ".jpg", imageDir);
+
+        picturePath = image.getAbsolutePath();
+
+        return image;
+    }
+
+    public void onClickIg(View view) {
         //Links to harportens instagram
         Uri uri = Uri.parse("http://instagram.com/_u/harportenvarberg");
         Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
