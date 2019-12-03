f8506c0403ef85af02bee421e732c0030d3e78b8
Oliver Lennartsson <oliverlennartsson@Olivers-MacBook-Air.local>
2019-12-03 10:30:38 +0100

    added final string variables DATABASE_NAME, TABLE_NAME, COL1 and COL2 and an oncreate method which creates the table. Updated onUpgrade with a drop table method

--- app/src/main/java/com/example/projecthairgate/DatabaseHelper.java
+++ app/src/main/java/com/example/projecthairgate/DatabaseHelper.java
@@ -8,17 +8,27 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    public static final String DATABASE_NAME = "userface.db";
+    public static final String TABLE_NAME = "saved_face";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "FACE_IMAGE";
+
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY, " +
+                "FACE_IMAGE BLOB) ";
+        db.execSQL(createTable);
+
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
     }
 }
