d5def302af5dc1a58b78f2787dd32f96d438ca9d
p0drickpayne <jakob@salomonssons.se>
2019-12-03 23:41:37 +0100

    Skapat grkidlayout istället för grdiview

--- app/src/main/java/com/example/projecthairgate/VartTeamActivity.java
+++ app/src/main/java/com/example/projecthairgate/VartTeamActivity.java
@@ -1,15 +1,29 @@
 package com.example.projecthairgate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class VartTeamActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vartteam_item);
+        setContentView(R.layout.activity_vartteam);
+
+        final ImageButton imageButton = findViewById(R.id.lars);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YoYo.with(Techniques.Bounce).duration(700).repeat(1).playOn(imageButton);
+            }
+        });
     }
     }
 
